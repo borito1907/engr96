5e160bfbd55b59dc53adf2daf20a8b86f8a4c743
borito1907 <62944527+borito1907@users.noreply.github.com>
2021-11-29 12:17:12 -0800

    Add files via upload
    File for free points

---
+++ playground.m
@@ -0,0 +1,16 @@
+
+
+clear all; clc; close all;
+
+figure(1)
+hold on
+
+% Add the lines
+% Create template for the drop zone
+
+
+title('Course')
+xlabel('Distance X (m)')
+ylabel('Distance Y (m)')
+grid on
+
