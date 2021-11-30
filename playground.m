58406828868a95d83ada793b3d3c072cf7aa8ef3
borito1907 <62944527+borito1907@users.noreply.github.com>
2021-11-29 16:09:01 -0800

    Update playground.m

--- playground.m
+++ playground.m
@@ -8,8 +8,14 @@ hold on
 % Add the lines
 % Create template for the drop zone
 % line([x1 x2], [y1 y2], 'linewidth',2)
-line([-2,-2],[0,13],'linewidth',2)
-
+line([-0.5715,0.5715],[0,0],'linewidth',2)
+line([-0.5715,-0.5715],[0,3.9624],'linewidth',2)
+line([0.5715,0.5715],[0,3.9624],'linewidth',2)
+line([0.5715,0.5715 + 0.6096],[3.9624,3.9624],'linewidth',2)
+line([-0.5715,-0.5715 - 0.6096],[3.9624,3.9624],'linewidth',2)
+line([-0.5715 - 0.6096,-0.5715 - 0.6096],[3.9624,3.9624 + 1.143],'linewidth',2)
+line([0.5715 + 0.6096,0.5715 + 0.6096],[3.9624,3.9624 + 1.143],'linewidth',2)
+line([-0.5715 - 0.6096,0.5715 + 0.6096],[3.9624 + 1.143,3.9624 + 1.143],'linewidth',2)
 
 title('Course')
 xlabel('Distance X (m)')
